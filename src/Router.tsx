302cd59d1c7e81d5c82769dc26d63c92e943a2bf
424on <88828652+424on@users.noreply.github.com>
2026-07-02 05:31:43 +0900

    chore: 디렉토리 구조 설계

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,31 +1,18 @@
-import {BrowserRouter, Routes, Route, Navigate,} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from "./pages/testlogin/Home";
-import Login from "./pages/testlogin/Login";
-import TokenProcessor from "./utils/Authorization/TokenProccesor";
+import Home from '~/pages/Home/Home';
+import Login from '~/pages/Auth/LoginPage';
+import TokenProcessor from '~/utils/Authorization/TokenProcessor';
 
 export default function Router() {
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    path="/"
-                    element={<Navigate to="/login" replace />}
-                />
-                <Route
-                    path="/login"
-                    element={<Login />}
-                />
-                <Route
-                    path="/loginwait"
-                    element={<TokenProcessor />}
-                />
-                <Route
-                    path="/home"
-                    element={<Home />}
-                />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/loginwait" element={<TokenProcessor />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
 import queryClient from "./QueryClient";
-import React from "react";
-import {AuthProvider} from "./contexts/Auth/AuthContext";
+import { AuthProvider } from "./contexts/Auth/AuthContext";
 import Router from "./Router";
 
 createRoot(document.getElementById("root")!).render(
-    <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-            <Router />
-        </AuthProvider>
-    </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  </QueryClientProvider>,
 );
