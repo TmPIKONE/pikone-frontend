4afda22098f766a220075a30e57f9c97b39dca7a
424on <88828652+424on@users.noreply.github.com>
2026-07-02 18:54:32 +0900

    fix: App.tsx, main.tsx 역할 분리 및 GlobalStyle 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,4 @@
-import { createRoot } from "react-dom/client";
-import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "./QueryClient";
-import { AuthProvider } from "./contexts/Auth/AuthContext";
-import Router from "./Router";
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
-  </QueryClientProvider>,
-);
+createRoot(document.getElementById('root')!).render(<App />);
